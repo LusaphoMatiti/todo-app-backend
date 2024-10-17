aeae609b2d2ae3fb09341f4f09758a55e0f56178
Lusapho Matiti <lusaphomatiti07@gmail.com>
2024-10-17 16:34:41 +0200

    updated CORS

--- server.js
+++ server.js
@@ -8,25 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "https://my-daily-task-dun.vercel.app", // Production frontend
-  "http://localhost:5173", // Local development
-];
-
 // Specify your frontend URL
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
   })
 );
 app.use(express.json());
