a4aa9608759da51a0708314118df683946a9a15c
Lusapho Matiti <lusaphomatiti07@gmail.com>
2024-10-21 16:12:00 +0200

    Updated

--- db.js
+++ db.js
@@ -2,16 +2,15 @@ import dotenv from "dotenv";
 import mysql from "mysql2/promise";
 
 dotenv.config();
-
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME,
-  connectionLimit: 10, // You can increase the connection limit if needed
-  waitForConnections: true,
-  connectTimeout: 10000, // Increase the timeout (in milliseconds)
-  queueLimit: 0,
+  host: process.env.DB_HOST || "autorack.proxy.rlwy.net", // Railway's host
+  user: process.env.DB_USER || "root", // Your MySQL user (root)
+  password: process.env.DB_PASSWORD || "", // Leave empty if there's no password
+  database: process.env.DB_NAME || "railway", // Ensure this matches your Railway DB name
+  port: process.env.DB_PORT || 57525, // Railway's port for MySQL
+  ssl: {
+    rejectUnauthorized: false, // To handle SSL issues
+  },
 });
 
 export default db;
